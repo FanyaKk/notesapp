d7bf767e7571cd3ba5317e77bff67a9ecac52174
FanyaKk <fania.karamfilova@gmail.com>
2022-02-28 20:05:46 +0200

    Add functionality for sorting notes table and get logged in username

--- header.php
+++ header.php
@@ -10,29 +10,36 @@
 </head>
 <body>
     <?php
+        include 'config.php';
         session_start();
         //check if the user is logged in or not
         if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
-        {
+        {     
     ?>
         <div class="header">
-                <a href="index.php" class="logo">Notes</a>
+            <a href="index.php" class="logo">Notes</a>
                 <div class="header-right">
                     <a class="active" href="index.php">Home</a>
                     <a href="login.php">Login</a>
                     <a href="register.php">Register</a>
                 </div>
-            </div>
+        </div>
     <?php
         }
         else
         {
+            $user_id = $_SESSION['user_id'];
+            $user = $connect->query("SELECT * FROM users WHERE user_id = $user_id");
+            $row = $user->fetch_array();
+            $username = $row['username'];
     ?>
             <div class="header">
-                <a href="#default" class="logo">Notes</a>
+                <a href="#default" class="logo">Notes</a>            
                 <div class="header-right">
+                    <a class="username">Welcome <?php echo $username ?></a>
                     <a class="active" href="index.php">Home</a>
                     <a href="logout.php">Logout</a>
+                    
                 </div>
             </div>
     <?php
--- index.php
+++ index.php
@@ -7,16 +7,33 @@
     <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 
+    <!-- INTEGRATE A TINYMCE FOR RICHTEXT EDITOR IN TEXTAREA -->
+    <script src="https://cdn.tiny.cloud/1/agfgtjjwpoqlys6lw2kj3my9amslaf5k1ps2raq6ggdkd95e/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script type="text/javascript">
+    tinymce.init({
+        selector:'textarea',
+        menubar: false,
+        content_style: '{ word-break: break-word; width: 100%; height: 35ch; border: 1px solid #cc9603; background: #e7e7e7; border-radius: 2px; outline: none; }'
+    });
+    </script>
+    <script src="sort-table.js"></script>
+
     <title>Welcome page</title>
 </head>
 <body>
-
+    
     <?php 
         include_once 'header.php';
         include 'welcome.inc.php';
         $search="";
+        
         //check if the user is logged in or not
-        if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true):
+        if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true): ?>
+        <div class="welcomeText">
+            <a>Welcome to our website.</br>Here you can add and manage your daily <span class="customText"> NOTES</span>!</a>
+        </div>
+        
+        <?php
         else: 
     ?>
     <div class="main-container">
@@ -48,7 +65,7 @@
             </div>
             
             <input class="title" name="title" value="<?php echo $title; ?>" placeholder="Title*" autofocus> </br>
-            <textarea class="contentField" name="contentField" placeholder="Content*"><?php echo $content; ?></textarea> </br>
+            <textarea id="content" class="contentField" name="contentField" placeholder="Content*"><?php echo $content; ?></textarea> </br>
             
             <?php if($update == true): ?>
                 <button type="submit" class="btnupdate" name="btnupdate" a href="index.php">Edit</button>
@@ -58,33 +75,36 @@
             <?php endif; ?>
         </form>
 
-    <!-- SEARCH BAR -->
-        <form class="searchBar" action="index.php" method="POST">
-            <input type="text" name="searchfield" placeholder="Search">
-            <button type="text" name="btnsearch">Search</button>
-        </form>
+        <!-- SEARCH BAR -->
+        <div class="searchContainer">
+            <form class="searchBar" action="index.php" method="POST">
+                <input class="searchfield" name="searchfield" placeholder="Search">
+                <button class="btnsearch" name="btnsearch">Search</button>
+            </form>
+        </div>
+        
 
     <!-- TABLE CONTAINS USER NOTES-->
         <div class="userdata">
             <h2 class="title">My notes</h2>
-            <table cellspacing="0" cellpadding="0">
+            <table class="js-sort-table" cellspacing="0" cellpadding="0">
+                
                 <tr>
                     <!-- <td>ID</td> -->
-                    <td class="header-table-title" width="50" >Date</td>
-                    <td class="header-table-title" width="50" >Title</td>
-                    <td class="header-table-title" width="150">Content</td>
-                    <td class="action header-table-title" >Action</td>
-                </tr>
-        </div>
-        
-                <script>
+                    <th class="header-table-title js-sort-date" width="50" >Date</th>
+                    <th class="header-table-title" width="50" >Title</th>
+                    <th class="header-table-title" width="150">Content</th>
+                    <th class="action header-table-title" >Action</th>
+                    
+                </tr>    
+                <!-- <script>
                     $(document).ready(function()
                     {
                         $("tr:odd").css({
                             "background-color":"rgb(141, 141, 141)",
                             "color":"#fff"});
                     });
-                </script>
+                </script> -->
 
                 <?php
                     // save user_id from current session in variable
@@ -106,7 +126,7 @@
                         echo "$numberOfResult search result found!";
                         while($row = mysqli_fetch_array($searchResults)): ?>
                         <tr>
-                            <!-- <td><?php echo $row['note_id']; ?></td> -->
+                        <!-- <td><?php echo $row['note_id']; ?></td> -->
                             <td><?php echo $row['date']; ?></td>
                             <td><?php echo $row['title']; ?></td>
                             <td><?php echo $row['content']; ?></td>
@@ -118,8 +138,7 @@
                                     <a href="index.php?delete=<?php echo $row['note_id']; ?>" class="delete_btn" name="delete_btn">Delete</a>
                                 </div>
                             </td>
-                        
-                        </tr>	
+                        </tr>                        	
                     <?php endwhile; 
                     } 
                     else{
@@ -141,12 +160,11 @@
                                     <a href="index.php?delete=<?php echo $row['note_id']; ?>" class="delete_btn" name="delete_btn">Delete</a>
                                 </div>
                             </td>
-                        
-                        </tr>	
+                        </tr>                        	
                     <?php endwhile;
                     } ?>
             </table>
-        <!--  -->
+        </div>
         <?php mysqli_close($connect); // Close connection ?>
     <?php endif; ?>
     </div>
